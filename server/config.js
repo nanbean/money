5b230c8885c6bcb745cc0d6abd9c54d9c5305005
nanbean <nanbean@nanbean.net>
2024-07-10 20:01:42 -0700

    Update config.js

--- server/config.js
+++ server/config.js
@@ -1,8 +1,13 @@
+const key = require('./key.json');
+
 const config = {
-	couchDBUrl: 'localhost:5984',
-	couchDBAdminId: 'admin',
-	couchDBAdminPassword: 'password',
-	sudoPassword: 'password'
+	couchDBUrl: process.env.REACT_APP_COUCHDB_URL,
+	couchDBAdminId: process.env.REACT_APP_COUCHDB_ADMIN_ID,
+	couchDBAdminPassword: process.env.REACT_APP_COUCHDB_ADMIN_PW,
+	calendarPrimaryEmail: process.env.REACT_APP_CALENDAR_PRIMARY_EMAIL,
+	reaitimeApiRul: process.env.REACT_APP_REALTIME_API_URL,
+	googleSpreadsheetDocId: process.env.REACT_APP_GOOGLE_SPREADSHEET_DOC_ID,
+	key
 };
 
 module.exports = config;
