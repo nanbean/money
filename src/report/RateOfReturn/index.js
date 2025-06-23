3a9585858de9dca6237d9bf403ff1b1d6396c87f
nanbean <nanbean@nanbean.net>
2025-06-23 07:29:27 -0700

    Fix: Improve null safety and layout structure in RateOfReturn component

--- src/report/RateOfReturn/index.js
+++ src/report/RateOfReturn/index.js
@@ -3,6 +3,7 @@ import { useDispatch, useSelector } from 'react-redux';
 
 import _ from 'lodash';
 
+import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 
 import ReportGrid from '../../components/ReportGrid';
@@ -16,13 +17,13 @@ import useReturnReport from './useReturnReport';
 
 export function RateOfReturn () {
 	const allAccountsTransactions = useSelector((state) => state.allAccountsTransactions);
-	const investementTransactions = allAccountsTransactions.filter(i => i.accountId.split(':')[1] === 'Invst');
-	const cashTransactions = allAccountsTransactions.filter(i => i.accountId.split(':')[2].match(/_Cash/));
+	const investementTransactions = allAccountsTransactions.filter(i => i.accountId?.split(':')[1] === 'Invst');
+	const cashTransactions = allAccountsTransactions.filter(i => i.accountId?.split(':')[2]?.match(/_Cash/));
 	const allInvestments = useSelector((state) => state.allInvestments);
 	const historyList = useSelector((state) => state.historyList);
 	const allInvestmentAccounts = Object.keys(_.groupBy(investementTransactions, 'account')).map(account => account);
 	const [filteredAccounts, setFilteredAccounts] = useState(allInvestmentAccounts);
-	const allCashAccounts = Object.keys(_.groupBy(cashTransactions, 'account')).map(account => account).filter(i => filteredAccounts.includes(i.split('_')[0]));
+	const allCashAccounts = Object.keys(_.groupBy(cashTransactions, 'account')).map(account => account).filter(i => i && filteredAccounts.includes(i.split('_')[0]));
 	const dispatch = useDispatch();
 
 	useEffect(() => {
@@ -36,7 +37,13 @@ export function RateOfReturn () {
 	const { reportData, geometricMean } = useReturnReport(allInvestments, allAccountsTransactions, investementTransactions, cashTransactions, historyList, filteredAccounts, allCashAccounts);
 
 	return (
-		<div>
+		<Box
+			sx={{
+				height: 'calc(100vh - 192px)',
+				display: 'flex',
+				flexDirection: 'column'
+			}}
+		>
 			<AccountFilter
 				allAccounts={allInvestmentAccounts}
 				filteredAccounts={filteredAccounts}
@@ -45,8 +52,10 @@ export function RateOfReturn () {
 			<Typography variant="subtitle1" gutterBottom>
 				{`Geometric Mean : ${((geometricMean - 1) * 100).toFixed(3)}%`}
 			</Typography>
-			{reportData.length > 0 && <ReportGrid reportData={reportData} />}
-		</div>
+			<Box sx={{ flex: 1, mt: 1 }}>
+				{reportData.length > 0 && <ReportGrid reportData={reportData} />}
+			</Box>
+		</Box>
 	);
 }
 
