45db414f17926908442b4df7835110239abebeab
nanbean <nanbean@nanbean.net>
2025-07-09 20:03:04 -0700

    Refactor: Enhance Transactions component to filter transactions by selected accounts

--- src/views/Transactions.js
+++ src/views/Transactions.js
@@ -1,4 +1,4 @@
-import React, { useMemo, useState } from 'react';
+import React, { useMemo, useState, useEffect, useRef } from 'react';
 import { useSelector } from 'react-redux';
 
 import Box from '@mui/material/Box';
@@ -12,14 +12,26 @@ import Container from '../components/Container';
 import BankTransactions from '../components/BankTransactions';
 import BankTransactionModal from '../components/BankTransactionModal';
 import SortMenuButton from '../components/SortMenuButton';
+import AccountFilter from '../components/AccountFilter';
 
-const filterTransactionsByDate = ( transactions, startDate, endDate ) =>{
+const filterTransactions = ( transactions, startDate, endDate, filteredAccounts ) => {
 	const validTypes = ['Bank', 'CCard', 'Cash'];
+
+	if (!filteredAccounts || filteredAccounts.length === 0) {
+		return [];
+	}
+
 	return transactions.filter(t => {
 		const accountId = t.accountId;
 		// accountId: 'account:Bank:XXX' or 'account:CCard:XXX' or 'account:Cash:XXX'
 		const type = accountId && accountId.split(':')[1];
 		if (!validTypes.includes(type)) return false;
+
+		const accountName = t.account;
+		if (!filteredAccounts.includes(accountName)) {
+			return false;
+		}
+
 		const date = moment(t.date);
 		return (!startDate || date.isSameOrAfter(moment(startDate), 'day')) &&
 				(!endDate || date.isSameOrBefore(moment(endDate), 'day'));
@@ -28,12 +40,15 @@ const filterTransactionsByDate = ( transactions, startDate, endDate ) =>{
 
 const Transactions = () => {
 	const allAccountsTransactions = useSelector(state => state.allAccountsTransactions);
+	const accountList = useSelector(state => state.accountList);
 	const [dateRange, setDateRange] = useState(() => {
 		const end = moment();
 		const start = moment().subtract(1, 'months');
 		return { start, end };
 	});
 	const [selectedRange, setSelectedRange] = useState('1m');
+	const [filteredAccounts, setFilteredAccounts] = useState([]);
+	const initialized = useRef(false);
 
 	const dateRangeOptions = [
 		{ value: '1m', label: '1 Month', getRange: () => ({ start: moment().subtract(1, 'months'), end: moment() }) },
@@ -43,9 +58,24 @@ const Transactions = () => {
 		{ value: 'all', label: 'All', getRange: () => ({ start: moment('2000-01-01'), end: moment() }) }
 	];
 
+	const allBankAccounts = useMemo(() => {
+		const validTypes = ['Bank', 'CCard', 'Cash'];
+		if (!accountList) return [];
+		return accountList
+			.filter(a => validTypes.includes(a.type))
+			.map(a => a.name);
+	}, [accountList]);
+
+	useEffect(() => {
+		if (!initialized.current && allBankAccounts.length > 0) {
+			setFilteredAccounts(allBankAccounts);
+			initialized.current = true;
+		}
+	}, [allBankAccounts]);
+
 	const filteredTransactions = useMemo(() => {
-		return filterTransactionsByDate(allAccountsTransactions, dateRange.start, dateRange.end);
-	}, [allAccountsTransactions, dateRange]);
+		return filterTransactions(allAccountsTransactions, dateRange.start, dateRange.end, filteredAccounts);
+	}, [allAccountsTransactions, dateRange, filteredAccounts]);
 
 	const handleDateChange = (start, end) => {
 		setDateRange({ start: moment(start), end: moment(end) });
@@ -76,6 +106,11 @@ const Transactions = () => {
 							value={selectedRange}
 							onChange={handleRangeChange}
 							options={dateRangeOptions}
+						/>
+						<AccountFilter
+							allAccounts={allBankAccounts}
+							filteredAccounts={filteredAccounts}
+							setfilteredAccounts={setFilteredAccounts}
 						/>
 					</Stack>
 					<Box sx={{ flex: 1, mb: 1, textAlign: 'center' }}>
